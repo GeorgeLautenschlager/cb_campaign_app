958cb17fe3d305b70ed3fc45c0e306f8ceda42ce
George Lautenschlager <georgelautenschlager@gmail.com>
2022-02-27 17:28:45 -0500

    fucking javascript man

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Briefing room loaded");
+  }
+
+  resize() {
+    console.log("Briefing room resized");
   }
 }
