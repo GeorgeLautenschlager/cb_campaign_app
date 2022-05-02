f3ee2c73497d9e807d4e5f4a95cfce47eb1f177f
George Lautenschlager <georgelautenschlager@gmail.com>
2022-05-01 23:03:31 -0400

    set proto_path for productions

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 module Constants
   PROTO_PATH = if Rails.env.production?
-    ''
+    '/mnt/c/Combat_Box/apollo/my-vl-campaign'
   elsif Rails.env.testing?
     'static_data/day-16/velikie-campaign.state'
   else
